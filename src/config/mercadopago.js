3cc709e58b7e10f7e085189279cc5ad419ce3c19
Igor <igoroliveira0150@gmail.com>
2025-04-19 20:16:56 -0300

    098

---
+++ src/config/mercadopago.js
@@ -0,0 +1,7 @@
+import mercadopago from 'mercadopago';
+
+mercadopago.configure({
+  access_token: "TEST-2223438209198426-040908-597c267bfdabae5f2befb939a4ac8d4d-1185888193"
+});
+
+export default mercadopago;

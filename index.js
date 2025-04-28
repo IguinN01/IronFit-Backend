c4ecb4364df979611e79da9ee1b8461cc0412d43
IronProjeto <ironfitacademia05@gmail.com>
2025-04-28 14:07:57 -0300

    146

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import mercadopago from './src/config/mercadopago.js';
 import authenticate from './src/plugins/authenticate.js';
 import pagamentoCreditoRoutes from './src/routes/pagamentoCredito.js';
 import freteRoutes from './src/routes/frete.js';
-import pedidosRoutes from './src/routes/pedidos';
+import pedidosRoutes from './src/routes/pedidos.js';
 
 import pg from 'pg';
 const { Pool } = pg;
